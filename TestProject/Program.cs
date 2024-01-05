b2cdd4436a1fdabc9d11294d470ff2fb3851ce7a
zephyrdark <7238474+zephyrdark@users.noreply.github.com>
2024-01-05 18:03:09 +0800

    continue lesson

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello C#!");
+
+// example of calling different methods in the .NET Class library
+Random dice = new Random();
+int roll = dice.Next(1,7);
+Console.WriteLine(roll);
+
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+dice.Next()
