9a48182f20bfda07a3d867cf19e0950998e86c25
zephyrdark <7238474+zephyrdark@users.noreply.github.com>
2024-01-22 20:38:57 +0800

    Continue exercise

--- gradingApplication/GuidedProject/Starter/Program.cs
+++ gradingApplication/GuidedProject/Starter/Program.cs
@@ -14,6 +14,8 @@ string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
 int[] studentScores = new int[10];
 
+string currentStudentLetterGrade = "";
+
 Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
@@ -42,7 +44,47 @@ foreach (string name in studentNames)
     }
 
     currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+
+        if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
 Console.WriteLine("Press the Enter key to continue");
