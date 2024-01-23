12f0990786463a3733cb89502d93de953f2b5def
zephyrdark <7238474+zephyrdark@users.noreply.github.com>
2024-01-23 16:02:09 +0800

    Complete exercise

---
+++ variableScope/Program.cs
@@ -0,0 +1,16 @@
+﻿//variable scope challenge
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+       found = true;
+}
+
+if (found) 
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
